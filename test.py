9bcc145b3e7437d19a2b75bfd6890e6aea56834f
chcppa <91139229+chcppa@users.noreply.github.com>
2023-04-19 19:57:41 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-model = joblib.load("model.pkl")
-
-print(model.predict(['Ты ужасна']))
-print(model.predict(['Ты прекрасна']))
-print(model.predict(['Ты плохой парень']))
-print(model.predict(['Ты хороший парень']))
